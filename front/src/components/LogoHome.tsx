365824437c59c5d1dc58367ad44fef46bc5280c8
Javier Cabezas <javiercp69@educastur.es>
2023-05-10 23:13:15 +0200

    Implementado el menu

--- front/src/components/LogoHome.tsx
+++ front/src/components/LogoHome.tsx
@@ -1,32 +1,31 @@
 import { Button } from "primereact/button";
 import { useNavigate } from "react-router-dom";
-import { Image } from 'primereact/image';
-import logo1 from '../img/logo1.png';
-import logoIcon from '../img/heart-pulse-solid.svg';
-import '../css/logohome.css';
+import { Image } from "primereact/image";
+import logo1 from "../img/logo1.png";
+import logoIcon from "../img/heart-pulse-solid.svg";
+
+import MenuButton from "./MenuButton";
+import "../css/logohome.css";
 export default function LogoHome() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate(); 
+  return (
+    <div>
+      <Button
+        className="p-buttonLogo p-button-rounded p-c boxlogo"
+        onClick={() => navigate("/")}
+        link
+      >
+        <Image
+          src={logoIcon}
+          width="28em"
+          className="p-button-icon logo"
+          alt="CoRAzón Virtual"
+          onClick={() => navigate("/")}
+        />
+      </Button>
 
-
-    return (
-        // <div >           
-                
-                <Button className="p-buttonLogo p-button-rounded p-c boxlogo" onClick={()=>navigate('/')} link>
-                        <Image 
-                                                         
-                               src={logoIcon}
-                               width="28em"
-                               className="p-button-icon logo"
-                               alt='CoRAzón Virtual'                      
-                               onClick={()=>navigate('/')} 
-                               />
-                        
-                   
-                   
-                </Button>
- 
-    //    </div>
-    );  
+      <MenuButton />
+    </div>
+  );
 }
-
