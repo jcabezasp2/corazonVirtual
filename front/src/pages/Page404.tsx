b88a715128bc16a6287f04fd89614bcbd955a4e3
Javier Cabezas <javiercp69@educastur.es>
2023-05-14 16:39:26 +0200

    Creada pagina de error 404

--- front/src/pages/Page404.tsx
+++ front/src/pages/Page404.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button } from 'primereact/button';
 import { useNavigate } from "react-router-dom";
-import './../css/Page404.css';
+import './../css/page404.css';
 
 class Iprops{
     
@@ -14,9 +14,9 @@ export default function Page404(props: Iprops){
         return(
             <div id="page404">
                 <h1 className="title404">404</h1>
-                <h3>Pagina no encontrada</h3>
-                <p>La pagina que buscas aun no existe o ha sido movida</p>
-                <Button label="Volver al inicio" icon="pi pi-home" onClick={() => navigate('/')} />
+                <h3 className="subHeader404">Pagina no encontrada</h3>
+                <p className="text404">La pagina que buscas aun no existe.</p>
+                <Button className="btn404" label="Volver al inicio" icon="pi pi-home" onClick={() => navigate('/')} />
             </div>
         )
     }
