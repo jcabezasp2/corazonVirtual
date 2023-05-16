f99b50ac6ddf3c4505b91cdcefabc9a2204598f1
Yeramontes <yeraimf78@educastur.es>
2023-05-16 11:39:19 +0200

    Modficada tabla herramientas(añadido campo imagen)

---
+++ back/aspnetapp/Migrations/20230506173032_5.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aspnetapp.Migrations
+{
+    /// <inheritdoc />
+    public partial class _5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Tools",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Tools");
+        }
+    }
+}
