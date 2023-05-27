6cbab4e56893a1ce6992b1940fc167e1c1c1f33d
Javier Cabezas <javiercp69@educastur.es>
2023-05-27 02:04:49 +0200

    correcion de error

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -74,7 +74,7 @@ function App() {
           <Route path="/estudiantes" element={user.role == Role.Teacher? <Students /> : <Home />} />
           <Route path="/herramientas" element={ <Tools />} />
           <Route path="/herramientas/formulario" element={user.role == Role.Teacher?<ToolForm /> : <Home /> } />
-          <Route path="/panel" element={user.role == Role.Teacher? <Panel /> : <Home />} />
+          <Route path="/panel" element={user.role != Role.Student? <Panel /> : <Home />} />
           <Route path="/admin/usuarios" element={user.role == Role.Admin? <Users /> : <Home />} />
           <Route path="/admin/roles" element={user.role == Role.Admin? <Roles /> : <Home />} />
           <Route path="/admin/permisos" element={user.role == Role.Admin? <Claims /> : <Home />} />
