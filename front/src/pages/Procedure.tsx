71c3fe3fc85ecb20c3caecca594b8f7fd738dd8d
Javier Cabezas <javiercp69@educastur.es>
2023-05-28 21:36:16 +0200

    Trabajo en vista de procedimiento

--- front/src/pages/Procedure.tsx
+++ front/src/pages/Procedure.tsx
@@ -1,27 +1,44 @@
 import React from "react";
 import { Button } from 'primereact/button';
+import Timeline from "../components/Timeline";
 import { useNavigate, useParams } from "react-router-dom";
 import { ProgressBar } from 'primereact/progressbar';
+import { appContext } from "../App";
 import "./../css/procedure.css";
 
-class Iprops{
-    
-    }
 
-export default function Procedure(props: Iprops){
+export default function Procedure(){
 
     const { id } = useParams();
 
     const navigate = useNavigate();
+    const context = React.useContext(appContext);
 
-        const [value, setValue] = React.useState(50);
-    
+        const [progress, setProgress] = React.useState(0);
+        const [steps, setSteps] = React.useState();
+        const [currentStep, setCurrentStep] = React.useState({});
+        const [currentStepIndex, setCurrentStepIndex] = React.useState(0);
+
+        React.useEffect(() => {
+        }, []);
+
+        const initialize = async () => {
+            const res = await context.apiCalls.getStepByProcedureId(id);
+            setSteps(res);
+        }
+
+        React.useEffect(() => {
+            initialize();
+        }, []);
+
+
+
+
         return(
             <div id="procedureView">
-              
+              <Timeline value={steps} selected={currentStepIndex}></Timeline>
 
-
-              <ProgressBar value={value}></ProgressBar>
+              <ProgressBar value={progress}></ProgressBar>
             </div>
         )
     }
