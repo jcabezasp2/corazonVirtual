bc9851b26ce2777428c8c2a476114423a9a52081
Javier Cabezas <javiercp69@educastur.es>
2023-05-30 07:59:03 +0200

    Solucionado error de login

---
+++ back/aspnetapp/Migrations/20230530053638_9.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aspnetapp.Migrations
+{
+    /// <inheritdoc />
+    public partial class _9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "LockoutEnabled",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "2",
+                column: "LockoutEnabled",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "3",
+                column: "LockoutEnabled",
+                value: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
