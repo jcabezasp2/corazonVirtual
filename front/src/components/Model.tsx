9bc650be0bde9b406f4b41bbbece7218841ee563
Javier Cabezas <javiercp69@educastur.es>
2023-06-03 06:36:45 +0200

    Correcion de errores y mejora en visualizacion de herramientas

--- front/src/components/Model.tsx
+++ front/src/components/Model.tsx
@@ -6,6 +6,7 @@ import * as constants from "./../assets/constants";
 
 interface Iprops {
   path: string;
+  scale: number;
 }
 
 
@@ -15,13 +16,12 @@ export default function Model(props: Iprops) {
 
     const context = React.useContext(appContext);
 
-
     const fbx = useFBX(`${constants.API_URL}images/${props.path}`);
 
     return(
         <mesh position={[0, 0, 0]} rotation={[0, Math.PI / 3, 0]} scale={[0.1, 0.1, 0.1]}>
         
-        <primitive object={fbx }  scale={0.1}/>
+        <primitive object={fbx }  scale={props.scale}/>
       </mesh>
       ); ;
 }
