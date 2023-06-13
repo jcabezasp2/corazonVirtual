ace75b86859ff2fd72b71c27cd943d3835a2774b
Lara-Herrero <OQI51428@educastur.es>
2023-06-13 13:55:26 +0200

    arreglando chart y user update

--- front/src/pages/Panel.tsx
+++ front/src/pages/Panel.tsx
@@ -1,121 +1,225 @@
 import React from "react";
 import { appContext } from "../App";
-import IStep from "../interfaces/Step";
-import Table from "../components/Table";
-import { Link } from "react-router-dom";
 import SubmitButton from "../components/form/SubmitButton";
 import { Avatar   } from 'primereact/avatar';
 import {FileUpload} from 'primereact/fileupload';
-import { Badge } from 'primereact/badge';
 import { useState, useEffect } from 'react';
 import { Chart } from 'primereact/chart';
 import InputTxt from "../components/form/InputTxt";
-import File from "../components/form/File";
 import '../css/panel.css';
 import InputPassword from "../components/form/InputPassword";
 import { Card } from 'primereact/card';
-import { Button } from 'primereact/button';
 import { Image } from 'primereact/image';
 import { Role, Icons } from "../assets/constants";
 import Icon from "./../components/Icons";
-
+import IPractice from "../interfaces/Practice";
+import { useParams} from 'react-router-dom';
+import { InputText } from "primereact/inputtext";
+import { Password } from "primereact/password";
 
 
 class Iprops { }
 
+
+
 export default function Panel(props: Iprops) {
 
+    
+    const { id } = useParams();
+    
     const context = React.useContext(appContext);
-    const [id, setId] = React.useState<string>('');
+    const [userId, setUserId] = React.useState<any>();
     const [user, setUser] = React.useState<string>('');
-    const [avatar, setAvatar] = React.useState<File>();
+    const [avatar, setAvatar] = React.useState<string>('');
     const [email, setEmail] = React.useState<string>('');
     const [password, setPassword] = React.useState<string>('');
     const [chartData, setChartData] = useState({});
     const [chartOptions, setChartOptions] = useState({});
-  
-   
+    const [titleGraph, settitleGraph] = React.useState<string>('');
+    const [practiceData, setpracticeData] = React.useState([]);  
+    const [stepsData, setstepsData] = React.useState([]);
+    const [procedureData, setprocedureData] = React.useState([]);
  
 
     const initialize = async () => {       
-        const res = await context.apiCalls.getMyUser();       
-       
-        setId(res.user.id);
+        const res = await context.apiCalls.getMyUser();   
+            
+        console.log("res", res.user.id)
+        console.log("user Id ", userId, "id params", id)
+        setUserId(res.user.id);
         setUser(res.user.userName);
         setEmail(res.user.email);
-        setPassword(res.user.password ? res.user.password :'aA1551-');
-      
+        setPassword(res.user.password);
+        setAvatar(res.user.photo);
+ 
     };
+    
 
+    const practices = async () => {       
+    let responsePractice = await context.apiCalls.getPracticeByUserId(userId);
+    console.log("responsePractice",responsePractice);
+    const practices = responsePractice.map((practice: IPractice) => {
+        return {
+          Id: practice.id,
+          Date: practice.date,
+          Duration: practice.duration,
+          IsFinished: practice.isFinished,
+          Observations: practice.observations,
+          Procedure: practice.procedure,
+          Step: practice.step,
+          UserId: practice.userId,
+        };
+      });
+      
+      setpracticeData(practices)
+        console.log("practicesss", practiceData)
+      let responseProcedure = await context.apiCalls.getProcedure(1);       
+      const procedures = await responseProcedure.json();
+      setprocedureData(procedures)
+      console.log("procedureData",procedures, "setprocedureData", procedureData);
+
+      const title = procedures.name;
+      settitleGraph(title);
+      console.log("title", title,"setTitle", titleGraph)
+      const pasos = procedures.steps;
+      setstepsData(pasos)
+      console.log("pasos",pasos, "stepsadata", stepsData)
+    } 
+
+
+        // const chartData = 
+    
+        //     {
+                
+        //         labels: pasos.id,                
+        //         datasets:
+        //         {practiceData.map((practice: IPractice) => practice.duration
+        //          [
+        //           {
+        //             label: procedureData.id,
+        //             data: practiceData.map((practice: IPractice) => practice.duration),
+        //             fill: false,
+        //             borderColor: getComputedStyle(document.documentElement).getPropertyValue('rgb(22, 28, 34)'),
+        //             tension: 0.5,
+        //           },
+        //         ],
+        //         )}
+        //       };
+        
+            //   const chartDataComponent = practiceData.map((item: any) => (
+            //     <Chart
+                 
+            //       type="line"
+            //       data={{
+            //         labels: [pasos.id],
+            //         datasets: [
+            //           {
+            //             label: item.id,
+            //             data: item.duration,
+            //             fill: true,
+            //             borderColor: getComputedStyle(document.documentElement).getPropertyValue('var(--primary-text-color)'),
+            //             tension: 0.4,
+            //           },
+            //         ],
+            //       }}
+            //     />
+            //   ));
+              
+            //   setChartData(chartData);
+
+
+      
+    
+  
     React.useEffect(() => {
        initialize();
+       practices();
     }, []);
 
     
-
-    const handleUser = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setUser(e.target.value);
-    }
-
-    const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setEmail(e.target.value);
-    }
-
-    const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(e.target.value);
-    }
+    
 
     const handleUpdateUser = async () => {
-        
-        const response = await context.apiCalls.updateUser(user, email, password);
+        console.log("user", user, "email", email,"avatar",avatar)
+        const response = await context.apiCalls.editUser(user, email, password, avatar);
         console.log(response);
     }
 
 
-    const handleEditImageuser = async () => {
-        // const response = await context.apiCalls.updateImageUser(avatar);
-        // console.log(response);
-        console.log("handle edit image user")
+    const handleAvatar = async ({ files }: any) => {  
+        const [file] = files;    
+    
+    if(avatar === ""){
+    const reader = new FileReader();
+    reader.onload = async (e: any) => {
+      let result = await context.apiCalls.uploadImageBase64(e.target.result);
+      setAvatar(result);
+        console.log("avatar vacio", avatar)       
+    };
+    reader.readAsDataURL(file);   
+    
+    }else{
+    let img = avatar.split("images/")
+    let deleteImg = img[1]
+     let res = await context.apiCalls.deleteImage(deleteImg);
+     if(res.ok){
+        console.log("delete",deleteImg)
+         }else{
+          console.log("no borra")
+         }
+    
+    
+    const reader = new FileReader();
+    reader.onload = async (e: any) => {
+      let result = await context.apiCalls.uploadImageBase64(e.target.result);    
+      setAvatar(result);
+      console.log("avatar borrado", avatar)  
+    };
+    reader.readAsDataURL(file);
     }
+    }
+    
+    
   
-    const footer = (
-        <div className="button-footer-datos">
-        <span id="button-datos" className=" col-6">
-           <SubmitButton
-                   
-                    onclik={handleUpdateUser}
-                    ctx={{id : id, user : user, email: email, password: password}}
-                    isLogin={true}
-                    
-                />
-        </span>
-        </div>
-    );
+    
+  
 
     useEffect(() => {
         const documentStyle = getComputedStyle(document.documentElement);
-        const textColor = documentStyle.getPropertyValue('var(--primary-color)');
-        const textColorSecondary = documentStyle.getPropertyValue('var(--text-color-secondary)');
-        const surfaceBorder = documentStyle.getPropertyValue('var(--surface-border)');
-        const data = {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            datasets: [
-                {
-                    label: 'First Dataset',
-                    data: [65, 59, 80, 81, 56, 55, 40],
-                    fill: false,
-                    borderColor: documentStyle.getPropertyValue('var(--primary-text-color)'),
-                    tension: 0.4
-                },
-                {
-                    label: 'Second Dataset',
-                    data: [28, 48, 40, 19, 86, 27, 90],
-                    fill: false,
-                    borderColor: documentStyle.getPropertyValue('var(--primary-color)'),
-                    tension: 0.4
-                }
-            ]
-        };
+        const textColor = documentStyle.getPropertyValue('var(--surface-700)');
+        const textColorSecondary = documentStyle.getPropertyValue('var(--surface-800)');
+        const surfaceBorder = documentStyle.getPropertyValue('var(--surface-900)');
+
+      
+                    // {
+                    //     label: 'Second Dataset',
+                    //     data: [element.score],
+                    //     fill: false,
+                    //     borderColor: documentStyle.getPropertyValue('var(--primary-color)'),
+                    //     tension: 0.4
+                    // }
+            //     ]
+            // );
+
+           
+            // labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            // datasets: [
+            //     {
+            //         label: 'First Dataset',
+            //         data: [65, 59, 80, 81, 56, 55, 40],
+            //         fill: false,
+            //         borderColor: documentStyle.getPropertyValue('var(--primary-text-color)'),
+            //         tension: 0.4
+            //     },
+            //     {
+            //         label: 'Second Dataset',
+            //         data: [28, 48, 40, 19, 86, 27, 90],
+            //         fill: false,
+            //         borderColor: documentStyle.getPropertyValue('var(--primary-color)'),
+            //         tension: 0.4
+            //     }
+            // ]
+        // };
         const options = {
             maintainAspectRatio: false,
             aspectRatio: 0.6,
@@ -146,9 +250,28 @@ export default function Panel(props: Iprops) {
             }
         };
 
-        setChartData(data);
+        // setChartData(chartData);
         setChartOptions(options);
     }, []);
+
+
+    const footer = (
+        <div className="button-footer-datos">
+        <span id="button-datos" className=" col-6">
+           <SubmitButton
+                   
+                    onclik={handleUpdateUser}
+                    ctx={{id : userId, user : user, email: email, password: password, avatar: avatar}}
+                    isLogin={true}
+                    
+                />
+        </span>
+        </div>
+    );
+
+
+
+
 
     return (
                  
@@ -156,16 +279,18 @@ export default function Panel(props: Iprops) {
 
                 
                 <div  className=" col-2">
-                    <Card className="avatar col-10" title={user.toUpperCase()} >                    
+                    <Card className="avatar col-12" title={user.toUpperCase()} >                    
                         <div className="card-avatar col-12">                       
-                            <div className="photo card flex justify-content-center ">
-                                {/* <Avatar id="photo" icon={context.user.role === Role.Student ? <Icon type={Icons.Student} text="Estudiante"/> : <Icon type={Icons.Teacher} text="Profesor"/>} shape="circle" size="xlarge"/> */}
+                            <div id="avatar-content" className="photo card flex justify-content-center ">
+                                
+                                {context.user.role=== Role.Student ?
+                                <Image id="photo" src={avatar === "" ? "./src/img/graduation-cap-solid.svg"  : avatar}  alt="Estudiante"/>
+                                :
+                                <Image id="photo" src={avatar === "" ? "./src/img/teacher.svg"  : avatar}  alt="Profesor"/>
+                                }
 
-                                {/* <Avatar id="photo" icon={context.user.role === Role.Student ? <Image src={Icons.Student} alt="Estudiante"/> : <Image src={Icons.Teacher} alt="Profesor"/>} shape="circle" size="xlarge"/> */}
-
-                                <Image id="photo" src={context.user.role === Role.Student ? "./src/img/graduation-cap-solid.svg"  : "./src/img/teacher.svg"} indicatorIcon="pi pi-pencil" alt={context.user.role === Role.Student ? "Estudiante" : "Profesor"}/>                  
-                                <FileUpload name="image" 
-                                // onSelect={onUpload}
+                                <FileUpload id="image" name="image"                                 
+                                onSelect={handleAvatar}
                                 mode="basic" accept="image/*" auto={true} />
                                 <label htmlFor="file"></label>
                             </div>
@@ -175,29 +300,76 @@ export default function Panel(props: Iprops) {
                 <div className="panelcontent col-12">
                     <Card className="col-3 card-panel datos" title="Datos"   footer={footer} >
                         <div className="flex card-form col-12">
-                            <label className="flex align-items-center col-2 " >Usuario</label>
-                                <div className="col-10">
-                                    <InputTxt labelname={user}
-                                        name={user} handleName={handleUser} />
-                                </div>
-                        </div>
-                        <div className="flex card-form col-12">
-                            <label className="flex align-items-center col-2 ">Email</label>
-                            <div className="col-10 align-self-start">
-                                <InputTxt labelname={email} name={email} handleName={handleEmail} />
-                            </div>
-                        </div>
-                        <div className="flex card-form col-12">
-                        <label className="flex align-items-center col-2">Password</label>
-                        <div className="col-10">
-                            <InputPassword labelname={"••••••••••"} name={password} handlePassword={handlePassword} />
-                        </div>
+                                <span className="p-inputgroup">
+                        <span className="p-inputgroup-addon">
+                        <i className="pi pi-user"></i>
+                        </span>
+                        
+                        <InputText
+                        className="inputtext"
+                        id="user"
+                        value={user}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                            setUser(e.target.value)
+                        }
+                        onKeyDown={(e : React.KeyboardEvent<HTMLInputElement>) => {
+                            if (e.key === "Enter" || e.key === "Tab") {
+                                setUser(e.currentTarget.value);
+                            }}}
+                        />
+                    
+                    </span>
+                       
+                        <span className="p-inputgroup">
+                        <span className="p-inputgroup-addon">
+                        <i className="pi pi-at"></i>
+                        </span>
+                            
+                            <InputText
+                        className="inputtext"
+                        id="email"
+                        value={email}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                            setEmail(e.target.value)
+                        }
+                        onKeyDown={(e : React.KeyboardEvent<HTMLInputElement>) => {
+                            if (e.key === "Enter" || e.key === "Tab") {
+                            setEmail(e.currentTarget.value);
+                            }}}
+                        />
+                        
+                         </span>  
+                        
+                        <span className="p-inputgroup">
+                        <span className="p-inputgroup-addon">
+                        <i className="pi pi-lock"></i>
+                        </span>
+                        <Password
+                        className="inputtext"
+                        id="password"
+                        value={password}
+                        onKeyDown={(e : React.KeyboardEvent<HTMLInputElement>) => {
+                            if (e.key === "Enter") {
+                            setPassword(e.currentTarget.value);
+                            context.apiCalls.login();
+                            }
+                        }}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                            setPassword(e.target.value)
+                        }
+                        toggleMask
+                        feedback={false}
+                        />
+                    
+                    </span>
+                       
                         </div>
                      </Card>                   
                   
                 
-                <Card className="col-5 card-panel chart" title="Gráfico prácticas" > 
-                    <Chart height="250%" type="line" data={chartData} options={chartOptions} />
+                <Card className="col-5 card-panel chart" title={titleGraph} > 
+                    {/* <Chart height="250%" type="line" data={chartData} options={chartOptions} /> */}
+                   
                 </Card>
                 </div>
             </div>          
