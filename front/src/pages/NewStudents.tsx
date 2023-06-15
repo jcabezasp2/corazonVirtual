64342f5c1f9216dd21d71e9a6ece7103b0e1d1cf
Yeramontes <yeraimf78@educastur.es>
2023-06-15 19:09:22 +0200

    Arreglado no se añaden usuarios al csv de descarga

--- front/src/pages/NewStudents.tsx
+++ front/src/pages/NewStudents.tsx
@@ -8,6 +8,7 @@ import { FileUpload } from 'primereact/fileupload';
 import { Dialog } from 'primereact/dialog';
 import { InputText } from 'primereact/inputtext';
 import { Navigate, useNavigate } from 'react-router-dom';
+import { Password } from 'primereact/password';
 
 export default function NewStudents() {
 
@@ -53,18 +54,20 @@ export default function NewStudents() {
         }
     }
 
-    const añadirUsuarios = () => {
+    const añadirUsuarios = async () => {
         const apikey = sessionStorage.getItem('apiKey');
-        let resultado = 'Nombre;Email;Password\n';
-        students.forEach(async (student) => {
+        let resultado = `Nombre;Email;Password\n`;
 
-            let res = await ctx.apiCalls.register(student[0], student[1], student[2]);
+        const genCsv = async () => {
+            for (const student of students) {
+                let res = await ctx.apiCalls.register(student[0], student[1], student[2]);
 
-            if (res.status == 201) {
-                resultado += `${student[0]};${student[1]};${student[2]}\n`;
+                if (res.status == 201) {
+                    resultado += `${student[0]};${student[1]};${student[2]}\n`;
+                }
             }
-
-        });
+        }
+        await genCsv()
         const blob = new Blob([resultado], { type: 'text/csv' });
         const url = window.URL.createObjectURL(blob);
         const a = document.createElement('a');
@@ -107,26 +110,26 @@ export default function NewStudents() {
         <div id='newStudents' className='flex flex-column justify-content-center'>
             <div>
                 <div className='flex flex-wrap gap-2 justify-content-between align-items-center'>
-                        <FileUpload mode="basic" auto name="demo[]"
-                            ref={fileUpload}
-                            customUpload uploadHandler={(e) => handleUpload(e)}
-                            chooseOptions={{
-                                icon: 'pi pi-upload',
-                                label: 'Seleccionar archivo'
-                            }}
-                        />
+                    <FileUpload mode="basic" auto name="demo[]"
+                        ref={fileUpload}
+                        customUpload uploadHandler={(e) => handleUpload(e)}
+                        chooseOptions={{
+                            icon: 'pi pi-upload',
+                            label: 'Seleccionar archivo'
+                        }}
+                    />
 
-                        <Button icon="pi pi-download" label='Plantilla' onClick={() => downloadPlantilla()} />
-                        <Button icon="pi pi-user-plus" label='Nuevo usuario' onClick={() => setModalVisible(true)} />
+                    <Button icon="pi pi-download" label='Plantilla' onClick={() => downloadPlantilla()} />
+                    <Button icon="pi pi-user-plus" label='Nuevo usuario' onClick={() => setModalVisible(true)} />
 
-                        <Button icon="pi pi-save" label='Añadir' onClick={añadirUsuarios} />
+                    <Button icon="pi pi-save" label='Añadir' onClick={añadirUsuarios} />
                 </div>
                 <Dialog header="Nuevo usuario" visible={modalVisible} style={{ width: '50vw' }} onHide={() => setModalVisible(false)}>
                     <div className='flex flex-row align-content-center'>
                         <div className='col-8 flex flex-column justify-content-center gap-2'>
                             <InputText placeholder='Nombre' onChange={(e) => setName(e.target.value)} />
                             <InputText placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
-                            <InputText placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
+                            <Password placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
                         </div>
                         <div className='col-4 flex justify-content-center align-items-center'>
                             <Button icon="pi pi-plus" onClick={() => {
