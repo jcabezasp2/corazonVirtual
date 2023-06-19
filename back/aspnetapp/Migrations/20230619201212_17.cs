369ae8c348eded60fbde8ebfa7f945990e8a0d56
Javier Cabezas <javiercp69@educastur.es>
2023-06-19 22:59:17 +0200

    Arreglas varios en vista de pacticas

---
+++ back/aspnetapp/Migrations/20230619201212_17.cs
@@ -0,0 +1,238 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aspnetapp.Migrations
+{
+    /// <inheritdoc />
+    public partial class _17 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Observations",
+                value: "Bien hecho" );
+            
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Observations",
+                value: "Intenta hacerlo mas rapido la proxima vez" );
+                        
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Observations",
+                value: "El esfuerzo y coraje no son suficientes sin propósito y dirección" );
+            
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Observations",
+                value: "El éxito se basa en su disposición a trabajar duro sin importar los obstáculos que se encuentren en su camino" );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Observations",
+                value: "Bien hecho" );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Observations",
+                value: "No hay viento favorable para el que no sabe a qué puerto se dirige" );
+            
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Observations",
+                value: "No es que tengamos poco tiempo, es que desperdiciamos mucho" );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Observations",
+                value: "Admira a quien lo intenta, aunque fracase" );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Observations",
+                value: "Admira a quien lo intenta, aunque fracase" );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Observations",
+                value: "Realiza cada una de tus acciones como si fuera la última de tu vida." );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "Observations",
+                value: "Ninguna pérdida debe sernos más sensible que la del tiempo, puesto que es irreparable" );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "Observations",
+                value: "Te conviertes en lo que le das a tu atención" );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "Observations",
+                value: "Las obras se tienen medio terminadas cuando se han comenzado bien" );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "Observations",
+                value: "No nos atrevemos a muchas cosas porque son difíciles, pero son difíciles porque no nos atrevemos a hacerlas." );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "Observations",
+                value: "Si logras algo bueno con trabajo duro, el trabajo pasa rápido, pero el bien perdura." );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "Observations",
+                value: "En cada momento mantén la atención en la tarea que tienes entre manos" );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "Observations",
+                value: "No obres como si fueras a vivir mil años; obra como si el fin estuviera muy cerca" );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "Observations",
+                value: "Comenzar es la mitad del trabajo, comienza nuevamente con la mitad restante, y habrás terminado" );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 19,
+                column: "Observations",
+                value: "Limítate a lo que tienes delante: la idea, la acción, la expresión." );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 20,
+                column: "Observations",
+                value: "Nada debe hacerse sin un propósito" );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 21,
+                column: "Observations",
+                value: "Incluso la cosa más pequeña debe hacerse con referencia a un fin" );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 22,
+                column: "Observations",
+                value: "Por la mañana, cuando te levantes sin ganas, que este pensamiento esté presente: Me estoy levantando para el trabajo de un ser humano" );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 23,
+                column: "Observations",
+                value: "Mañana no es nada, hoy es demasiado tarde; el bien vivió ayer" );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 24,
+                column: "Observations",
+                value: "La suerte favorece a la mente preparada" );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 25,
+                column: "Observations",
+                value: "El buen médico conoce a sus pacientes de arriba abajo, y su conocimiento se compra caro" );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 26,
+                column: "Observations",
+                value: "Si logras algo bueno con trabajo duro, el trabajo pasa rápido, pero el bien perdura." );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 27,
+                column: "Observations",
+                value: "No tengas miedo al trabajo duro. Nada valioso viene fácilmente." );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 28,
+                column: "Observations",
+                value: "El médico no debe tratar la enfermedad sino al paciente que la padece" );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 29,
+                column: "Observations",
+                value: "La medicina moderna es un crisol de técnica, ciencia y humanismo" );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 30,
+                column: "Observations",
+                value: "La medicina es la ciencia de la incertidumbre y el arte de la probabilidad" );
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 31,
+                column: "Observations",
+                value: "La medicina es una profesión en la que la gente confía con su vida" );
+        
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
