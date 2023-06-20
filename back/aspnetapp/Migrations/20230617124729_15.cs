c687407767bcd2cca1fda390493df557e3928c33
Javier Cabezas <javiercp69@educastur.es>
2023-06-20 07:03:59 +0200

    Arreglada migracion

--- back/aspnetapp/Migrations/20230617124729_15.cs
+++ back/aspnetapp/Migrations/20230617124729_15.cs
@@ -21,10 +21,10 @@ namespace aspnetapp.Migrations
                 {
                      
                     
-                     {"6", "user6", "USER6", "user6@example.es", "USER6@EXAMPLE.ES", false, "AQAAAAIAAYagAAAAEE0A7Ewgd3h9osBEANSfNlWshhxLJ0L96PtmV+dmHyggq5njh+VwUKXPRSLvs6jySw==", null, null, null, false, false, null,false, 0},
-                     {"7", "user7", "USER7", "user7@example.es", "USER7@EXAMPLE.ES",  false, "AQAAAAIAAYagAAAAEE0A7Ewgd3h9osBEANSfNlWshhxLJ0L96PtmV+dmHyggq5njh+VwUKXPRSLvs6jySw==", null, null, null, false, false, null,false, 0},
-                     {"8", "user8", "USER8", "user8@example.es", "USER8@EXAMPLE.ES", false, "AQAAAAIAAYagAAAAEE0A7Ewgd3h9osBEANSfNlWshhxLJ0L96PtmV+dmHyggq5njh+VwUKXPRSLvs6jySw==", null, null, null, false, false, null,false, 0},
-                     {"9", "user9", "USER9", "user9@example.es", "USER9@EXAMPLE.ES",  false, "AQAAAAIAAYagAAAAEE0A7Ewgd3h9osBEANSfNlWshhxLJ0L96PtmV+dmHyggq5njh+VwUKXPRSLvs6jySw==", null, null, null, false, false, null,false, 0},
+                     {"996", "user6", "USER6", "user6@example.es", "USER6@EXAMPLE.ES", false, "AQAAAAIAAYagAAAAEE0A7Ewgd3h9osBEANSfNlWshhxLJ0L96PtmV+dmHyggq5njh+VwUKXPRSLvs6jySw==", null, null, null, false, false, null,false, 0},
+                     {"997", "user7", "USER7", "user7@example.es", "USER7@EXAMPLE.ES",  false, "AQAAAAIAAYagAAAAEE0A7Ewgd3h9osBEANSfNlWshhxLJ0L96PtmV+dmHyggq5njh+VwUKXPRSLvs6jySw==", null, null, null, false, false, null,false, 0},
+                     {"998", "user8", "USER8", "user8@example.es", "USER8@EXAMPLE.ES", false, "AQAAAAIAAYagAAAAEE0A7Ewgd3h9osBEANSfNlWshhxLJ0L96PtmV+dmHyggq5njh+VwUKXPRSLvs6jySw==", null, null, null, false, false, null,false, 0},
+                     {"999", "user9", "USER9", "user9@example.es", "USER9@EXAMPLE.ES",  false, "AQAAAAIAAYagAAAAEE0A7Ewgd3h9osBEANSfNlWshhxLJ0L96PtmV+dmHyggq5njh+VwUKXPRSLvs6jySw==", null, null, null, false, false, null,false, 0},
                     
                         
                     
@@ -36,12 +36,10 @@ namespace aspnetapp.Migrations
                 columns: new[] { "UserId", "RoleId" },
                 values: new object[,]
                 {
-                    { "4", "3" },
-                    { "5", "3" },
-                    { "6", "3" },
-                    { "7", "3" },
-                    { "8", "3" },
-                    { "9", "3" }
+                    { "996", "3" },
+                    { "997", "3" },
+                    { "998", "3" },
+                    { "999", "3" }
                   
                 }
             );
@@ -54,34 +52,34 @@ namespace aspnetapp.Migrations
                     { new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 40, 1,"3", 1, false },
                     { new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 30, 1,"2", 2, false },
                     { new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 20, 1,"3", 3, false },
-                    { new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 15, 1,"8", 4, false },
+                    { new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 15, 1,"998", 4, false },
                     { new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 25, 1,"3", 5, false },
                     { new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 50, 1,"3", 6, false },
-                    { new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1,"1", 7, false },
-                    { new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 23, 1,"5", 8, false },
+                    { new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1,"996", 7, false },
+                    { new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 23, 1,"996", 8, false },
                     { new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1,"3", 9, false },
-                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1, "2",10, false },
-                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1, "7",10, false },
-                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 45, 1, "9",12, false },
-                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1, "6",13, false },
-                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 55, 1, "1",14, false },
+                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1, "997",10, false },
+                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1, "997",10, false },
+                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 45, 1, "999",12, false },
+                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1, "996",13, false },
+                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 55, 1, "998",14, false },
                     {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 35, 1, "3",15, false },
-                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 26, 1, "4",16, false },
-                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 42, 1, "1",17, false },
-                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 18, 1, "8", 8, false },
-                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 34, 1, "8", 9 , false },
-                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 66, 1, "7",10, false },
-                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 54, 1,"1", 1, false },
-                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1,"4", 3, false },
+                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 26, 1, "997",16, false },
+                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 42, 1, "997",17, false },
+                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 18, 1, "998", 8, false },
+                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 34, 1, "998", 9 , false },
+                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 66, 1, "997",10, false },
+                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 54, 1,"998", 1, false },
+                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1,"997", 3, false },
                     {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1,"3", 2, false },
-                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1,"2", 6, false },
-                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1,"6", 8, false },
-                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1, "4",16, false },
-                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1, "8",13, false },
-                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1,"1", 5, false },
-                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1, "1",14, false },
-                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1,"5", 12,  false },
-                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1, "2",10, false },
+                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1,"998", 6, false },
+                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1,"996", 8, false },
+                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1, "996",16, false },
+                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1, "998",13, false },
+                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1,"997", 5, false },
+                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1, "998",14, false },
+                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1,"996", 12,  false },
+                    {  new DateTime(2021, 6, 9, 0, 0, 0, DateTimeKind.Utc), "Observations", 60, 1, "998",10, false },
 
                 }                                 
               
